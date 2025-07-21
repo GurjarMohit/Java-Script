bca256d5e0b76e93149fb5e827fc81c59a90530a
Mohit Gurjar <138752644+GurjarMohit@users.noreply.github.com>
2025-07-21 06:12:16 +0000

    variables commited

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=45433
+let email="mohit@133";
+var name="mohit";
+accountcity="Indore";
+
+// you can not reassign the const a variable
+//accountId=123;
+/*
+prefer not to use var becouse of block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([email,name,accountcity]);
